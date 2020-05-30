24e909cbf1fd96aeb74da5949adc42aa7b34742c
Marco Acierno <marcoaciernoemail@gmail.com>
2020-05-30 21:29:13 +0000

    Fix asgi view to allow multiple subscriptions

--- strawberry/asgi/__init__.py
+++ strawberry/asgi/__init__.py
@@ -5,7 +5,7 @@ from graphql import GraphQLError, GraphQLSchema
 from graphql.error import format_error as format_graphql_error
 from starlette.requests import Request
 from starlette.types import Receive, Scope, Send
-from starlette.websockets import WebSocket, WebSocketState
+from starlette.websockets import WebSocket, WebSocketState, WebSocketDisconnect
 
 from ..graphql import execute, subscribe
 from ..utils.debug import pretty_print_graphql_operation
@@ -60,27 +60,57 @@ class GraphQL:
     async def handle_websocket(self, scope: Scope, receive: Receive, send: Send):
         websocket = WebSocket(scope=scope, receive=receive, send=send)
 
+        subscriptions: Dict[str, AsyncGenerator] = {}
+        tasks = {}
+
         await websocket.accept(subprotocol="graphql-ws")
 
-        while websocket.application_state != WebSocketState.DISCONNECTED:
-            message = await websocket.receive_json()
+        try:
+            while (
+                websocket.client_state != WebSocketState.DISCONNECTED
+                and websocket.application_state != WebSocketState.DISCONNECTED
+            ):
+                message = await websocket.receive_json()
 
-            operation_id = message.get("id")
-            message_type = message.get("type")
+                operation_id = message.get("id")
+                message_type = message.get("type")
 
-            if message_type == GQL_CONNECTION_INIT:
-                await websocket.send_json({"type": GQL_CONNECTION_ACK})
+                if message_type == GQL_CONNECTION_INIT:
+                    await websocket.send_json({"type": GQL_CONNECTION_ACK})
 
-                if self.keep_alive:
-                    asyncio.create_task(self.handle_keep_alive(websocket))
-            elif message_type == GQL_CONNECTION_TERMINATE:
-                await websocket.close()
-            elif message_type == GQL_START:
-                await self.start_subscription(
-                    message.get("payload"), operation_id, websocket
-                )
-            elif message_type == GQL_STOP:
-                await websocket.close()
+                    if self.keep_alive:
+                        self._keep_alive_task = asyncio.create_task(
+                            self.handle_keep_alive(websocket)
+                        )
+                elif message_type == GQL_CONNECTION_TERMINATE:
+                    await websocket.close()
+                elif message_type == GQL_START:
+                    async_result = await self.start_subscription(
+                        message.get("payload"), operation_id, websocket
+                    )
+
+                    subscriptions[operation_id] = async_result
+
+                    tasks[operation_id] = asyncio.create_task(
+                        self.handle_async_results(async_result, operation_id, websocket)
+                    )
+                elif message_type == GQL_STOP:
+                    if operation_id not in subscriptions:
+                        return
+
+                    await subscriptions[operation_id].aclose()
+                    tasks[operation_id].cancel()
+                    del tasks[operation_id]
+                    del subscriptions[operation_id]
+        except WebSocketDisconnect:
+            pass
+        finally:
+            if self._keep_alive_task:
+                self._keep_alive_task.cancel()
+
+            for operation_id in subscriptions:
+                await subscriptions[operation_id].aclose()
+                tasks[operation_id].cancel()
 
     async def start_subscription(self, data, operation_id: str, websocket: WebSocket):
         query = data["query"]
@@ -90,18 +120,25 @@ class GraphQL:
         if self.debug:
             pretty_print_graphql_operation(operation_name, query, variables)
 
-        data = await subscribe(
-            self.schema, query, variable_values=variables, operation_name=operation_name
+        return await subscribe(
+            self.schema,
+            query,
+            variable_values=variables,
+            operation_name=operation_name,
         )
 
+    async def handle_async_results(
+        self, results: typing.AsyncGenerator, operation_id: str, websocket: WebSocket
+    ):
         try:
-            async for result in data:
+            async for result in results:
                 payload = {"data": result.data}
 
                 if result.errors:
                     payload["errors"] = [
                         format_graphql_error(err) for err in result.errors
                     ]
+
                 await self._send_message(websocket, GQL_DATA, payload, operation_id)
         except Exception as error:
             if not isinstance(error, GraphQLError):
@@ -114,12 +151,11 @@ class GraphQL:
                 operation_id,
             )
 
-        await self._send_message(websocket, GQL_COMPLETE, None, operation_id)
-
-        if self._keep_alive_task:
-            self._keep_alive_task.cancel()
-
-        await websocket.close()
+        if (
+            websocket.client_state != WebSocketState.DISCONNECTED
+            and websocket.application_state != WebSocketState.DISCONNECTED
+        ):
+            await self._send_message(websocket, GQL_COMPLETE, None, operation_id)
 
     async def _send_message(
         self,
